e33052578c72460129cfab511624d9fdcdb0d8b1
codebyhannah <firekatburns@gmail.com>
2023-09-21 20:40:57 -0600

    week 1 and week 2 practice

---
+++ week02/w2Practice.js
@@ -0,0 +1,50 @@
+// 1. Add two numbers
+var a = 2 + 3; // 5
+console.log(a);
+
+// 2. Multiply two numbers
+var b = 2 * 3; // 6
+console.log(b);
+
+// 3. Subtract one from a number
+var c = 3 - 1; // 2
+console.log(c);
+
+// 4. concatinate two strings together.
+var d = "hello " + "world"; // hello world
+console.log(d);
+
+// 5. assign a value to a variable
+var e = 5; // 5
+console.log(e);
+var e = 7; // 7
+console.log(e);
+
+// 6. increment the value in a variable by 3
+var f = 6 + 3; // 9
+console.log(f);
+
+// 7. compare two values to see if they are the same
+var g = 5 == 6; // false
+console.log(g);
+var h = 5 == 5; // true
+console.log(h);
+
+// 8. check to see if one number is less than another number
+var i = 7 < 10; // true
+console.log(i);
+var j = 10 < 7; // false
+console.log(j);
+
+// 9. Check to see if two values are NOT equal
+var k = 4 != 4; // false
+console.log(k);
+var l = 4 != 3; // true
+console.log(l);
+// 10. Check to see if a value is less than 10 and greater than 0
+var m = 5;
+var n = m < 10 && m > 0; // true
+console.log(n);
+var o = -1;
+var p = o < 10 && o > 0; // false
+console.log(p);
